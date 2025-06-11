2fe3857d1485b71a85b9411fcdccb1bc4d73263b
dkijania <dariusz@o1labs.org>
2025-06-11 22:40:08 +0200

    fetch tags

--- ci/scripts/helpers.sh
+++ ci/scripts/helpers.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 function export_git_env_vars() {
+    git fetch --tags --quiet
+    if [ $? -ne 0 ]; then
+        echo "Failed to fetch Git tags. Ensure you are in a valid Git repository."
+        exit 1
+    fi
     GIT_TAG=$(git describe --tags --abbrev=0)
     GIT_COMMIT=$(git rev-parse --short HEAD)
     if [ -z "$GIT_TAG" ]; then
